c678a830f931ec7ee5dff368387f5e1c42f27b45
Adarsh Kumar Yadav <alokyadav1139@gmail.com>
2024-12-05 21:37:33 +0530

    update

--- script.js
+++ script.js
@@ -1,31 +1,69 @@
-function hamburg(){
-    const navbar = document.querySelector(".dropdown")
-    navbar.style.transform = "translateY(0px)"
+/*SKILLS*/
+const skillsContent = document.getElementsByClassName('skills_content'),
+      skillsHeader = document.querySelectorAll('.skills_header')
+
+function toggleSkills(){
+    let itemclass = this.parentNode.className
+
+    for ( i=0; i<skillsContent.length; i++){
+        skillsContent[i].className = 'skills_content skills_close'
+    }
+    if(itemclass === 'skills_content skills_close'){
+        this.parentNode.className = 'skills_content skills_open'
+    }
 }
-function cancel(){
-    const navbar = document.querySelector(".dropdown")
-    navbar.style.transform = "translateY(-500px)"
-}
+
+skillsHeader.forEach((el)=>{
+    el.addEventListener('click', toggleSkills)
+})
+
+/*QUALIFICATION*/
+
+const tabs = document.querySelectorAll('[data-target]'),
+      tabContents = document.querySelectorAll('[data-content]')
+
+tabs.forEach(tab =>{
+    tab.addEventListener('click', ()=>{
+        const target = document.querySelector(tab.dataset.target)
+
+        tabContents.forEach(tabContent =>{
+            tabContent.classList.remove('qualification_active')
+
+        })
+        target.classList.add('qualification_active')
+
+        tabs.forEach(tab =>{
+            tab.classList.remove('qualification_active')
+        })
+        tab.classList.add('qualification_active')
+    })
+})
+
 // Typewriter Effect
+
 const texts = [
     "DEVELOPER",
     "DESIGNER",
     "YOUTUBER"
 ]
+
 let speed  =100;
 const textElements = document.querySelector(".typewriter-text");
+
 let textIndex = 0;
 let charcterIndex = 0;
+
 function typeWriter(){
     if (charcterIndex < texts[textIndex].length){
         textElements.innerHTML += texts[textIndex].charAt(charcterIndex);
         charcterIndex++;
-        setTimeout(typeWriter, speed);
+        setTimeout(typeWriter, speed); 
     }
     else{
         setTimeout(eraseText, 1000)
     }
 }
+
 function eraseText(){
     if(textElements.innerHTML.length > 0){
         textElements.innerHTML = textElements.innerHTML.slice(0,-1);
@@ -37,4 +75,5 @@ function eraseText(){
         setTimeout(typeWriter, 500)
     }
 }
+
 window.onload = typeWriter
